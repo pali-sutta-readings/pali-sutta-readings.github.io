9761069a7c1ed83ade8e0e1176fed5549e901411
Gambhiro <gambhiro@users.noreply.github.com>
2025-03-02 14:10:14 +0000

    upd

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,8 +4,21 @@ set -e
 
 # Source branch
 
+git pull origin source
 git add -A .
 git status
 
 git commit -m "upd"
 git push origin source
+
+make build
+
+# Main branch
+
+cd ../pali-sutta-readings.github.io-main/
+git pull origin main
+git add -A .
+git status
+
+git commit -m "upd"
+git push origin main
